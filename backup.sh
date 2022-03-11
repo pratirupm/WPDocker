b1abbef654c4c7ad234e419c0a0e99f75f77f952
pratirupm <pratirupm@gmail.com>
2022-03-11 15:20:11 +0530

    Init

--- backup.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-docker compose run --user 33:33 --rm wpcli db export ./wp-content/backup.sql
--- import.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-FILE=./contents/backup.sql
-if [ -f "$FILE" ]; then
-    echo "$FILE exists."
-    docker-compose run --user 33:33 --rm wpcli db import ./wp-contents/backup.sql
-else 
-    echo "$FILE does not exist."
-fi
-
